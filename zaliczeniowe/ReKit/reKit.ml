fd04535b99b1d5112e9fb9cabce3d4c761eac2f7
Juliusz Pawlus <60894769+Bladdyy@users.noreply.github.com>
2025-01-11 15:33:39 +0100

    Update reKit.ml

--- zaliczeniowe/ReKit/reKit.ml
+++ zaliczeniowe/ReKit/reKit.ml
@@ -3,7 +3,7 @@ type t = Regextkit.Tree.re;;
 
 (** Parser powinien być w stanie sparsować wyrażenia regularne używające 
   składni obowiązującej w zadaniu. *)
-let re str = Regextkit.Re.parse str;;
+let re str = Regextkit.Re.parse (String.map (fun c -> if c = '|' then '+' else c) str);; (* Changing '|' to '+' then parsing.*)
 
 (** Drukowanie reprezentacji wyrażeń regularnych na stdout; 
   na końcu powinno być przejście do nowego wiersza. *)
@@ -11,11 +11,11 @@ let debug regex = Regextkit.Re.print regex;;
 
 (** Sprawdzanie czy {b cały} napis pasuje do wyrażenia regularnego. *)
 let matches regex str = 
-  let rec der reg s = 
-    let len = String.length s
-    in
-    if len = 0 then reg
-    else der (Regextkit.Re.derivative (reg) (String.make 1 s.[0])) (String.sub str 1 (len - 1))
+  let lens = String.length str 
+  in
+  let rec der reg ind = (* Derivating char by char *)
+    if ind = lens then reg
+    else der (Regextkit.Re.simplify(Regextkit.Re.derivative reg (String.make 1 str.[ind]))) (ind + 1)
   in 
-  Regextkit.Re.is_nullable (der regex str);;
+  Regextkit.Re.is_nullable (der regex 0);; (* Check if end regex nullable. *)
 
