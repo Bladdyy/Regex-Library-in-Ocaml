b7a0043ac003099460632e0b1d7849c8708a771b
Juliusz Pawlus <60894769+Bladdyy@users.noreply.github.com>
2025-01-11 18:18:07 +0100

    Update main.ml

--- zaliczeniowe/Main/main.ml
+++ zaliczeniowe/Main/main.ml
@@ -1,6 +1,5 @@
 open TestRe
 open Regexp
-(* Moduł zimplementowany dla SimpleRegexp *)
 
 module Simple: REGEXP = struct
   include ReSimple
@@ -14,11 +13,10 @@ module SimpleTester = TestRegexp.Test(Simple)
 module KitTester = TestRegexp.Test(Kit)
 
 let () =
-  (* Uruchamianie testów dla SimpleRegexp *)
-  Printf.printf "Running tests for SimpleRegexp...\n";
+  Printf.printf "Running tests for Simple...\n\n";
   let errors, time = SimpleTester.test () in
   Printf.printf "Test completed with %d errors in %.2f seconds\n" errors time;
-  Printf.printf "---------------------------------------------------\n\n";
-  Printf.printf "Running tests for KitRegexp...\n";
+  Printf.printf "---------------------------------------------------\n";
+  Printf.printf "Running tests for Kit...\n\n";
   let errors, time = KitTester.test () in
   Printf.printf "Test completed with %d errors in %.2f seconds\n" errors time
